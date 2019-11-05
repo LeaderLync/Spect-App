9fde4900694f7c7ecb7c8e4bff953e44832042cc
Marc Diaz <marcgabe15@gmail.com>
2019-11-05 11:02:28 -0500

    first login commit

---
+++ client/src/config/config.js
@@ -0,0 +1,10 @@
+export default firebaseConfig = {
+    apiKey: "AIzaSyA5Y4iTvOjaSnERkJ0cguzQogysKr5MQwA",
+    authDomain: "spect-app.firebaseapp.com",
+    databaseURL: "https://spect-app.firebaseio.com",
+    projectId: "spect-app",
+    storageBucket: "spect-app.appspot.com",
+    messagingSenderId: "951920210721",
+    appId: "1:951920210721:web:dd64d37d40d4c5063bc460",
+    measurementId: "G-F78HE0E4FY"
+  };
