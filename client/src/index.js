a7a9b268a1876858ab07af72b3092df2cae262ad
Tanner Ropp <tanner.ropp@ufl.edu>
2019-11-07 00:26:05 -0500

    added contact info input fields

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import App from './App';
 
 ReactDOM.render(
--- client/src/views/StudentSurvey/StudentSurvey.js
+++ client/src/views/StudentSurvey/StudentSurvey.js
@@ -5,10 +5,38 @@ import SoftSkill from '../../components/SoftSkill/SoftSkill.js'
 class StudentSurvey extends React.Component {
   render() {
     return (
-      <div>
-        <SoftSkill abbrev='Le' skill='Leadership'/>
-        <SoftSkill abbrev='Cr' skill='Creativity'/>
-        <SoftSkill abbrev='Mi' skill='Mindfulness'/>
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
+            <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+              <div className="card-body">
+                <h3>Contact Information</h3>
+                <form>
+                  <div className="form-row">
+                    <div className="form-group col">
+                      <label for="inputFirstName">First name</label>
+                      <input type="text" className="form-control" placeholder="First name"/>
+                    </div>
+                    <div className="form-group col">
+                      <label for="inputLastName">Last name</label>
+                      <input type="text" className="form-control" placeholder="Last name"/>
+                    </div>
+                  </div>
+                  <div className="form-row">
+                    <div className="form-group col">
+                      <label for="inputEmail">Email</label>
+                      <input type="email" className="form-control" placeholder="Email"/>
+                    </div>
+                    <div className="form-group col">
+                      <label for="inputNumber">Telephone number</label>
+                      <input type="tel" className="form-control" placeholder="(xxx)xxx-xxxx"/>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
