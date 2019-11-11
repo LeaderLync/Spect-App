1c911b0141ee0c176abb9b559add9cd6be70902e
tjerd <tylerjerd9@gmail.com>
2019-11-10 20:25:35 -0500

    Revert "Added Navbar and Companyard components"
    This reverts commit 6b03a03266becb46896ea08800d84237a6034ad9.

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,12 @@ import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home"
 import NotFound from "./views/NotFound"
 import Header from "./components/Header/Header"
-import Navbar from "./components/Navbar/Navbar"
-import CompanyCard from "./components/CompanyCard"
 
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <CompanyCard />
+      <Header />
       <Switch>
         <Route exact path="/Home" component={Home} />
         <Route exact path="/">
