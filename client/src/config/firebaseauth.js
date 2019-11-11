7fdac9f808544d019cf99df099950e108a267ee9
Marc Diaz <marcgabe15@gmail.com>
2019-11-11 17:20:59 -0500

    input config for deployment

--- client/src/config/firebaseauth.js
+++ client/src/config/firebaseauth.js
@@ -1,16 +1,16 @@
 import firebase from 'firebase'
 
 const appconfig = {
-    apiKey: (process.env.API_KEY),
-    authDomain: (process.env.AUTH_DOMAIN),
-    databaseURL: (process.env.DATABASE_URL),
-    projectId: (process.env.PROJECT_ID),
-    storageBucket: (process.env.STORAGE_BUCKET),
-    messagingSenderId:(process.env.MESSAGING_SENDER_ID),
-    appId: (process.env.APP_ID),
-    measurementId: (process.env.MEASUREMENT_ID)
+    apiKey: (process.env.API_KEY || require('./config').default.apiKey),
+    authDomain: (process.env.AUTH_DOMAIN || require('./config').default.authDomain),
+    databaseURL: (process.env.DATABASE_URL || require('./config').default.databaseURL),
+    projectId: (process.env.PROJECT_ID || require('./config').default.projectId),
+    storageBucket: (process.env.STORAGE_BUCKET || require('./config').default.storageBucket),
+    messagingSenderId:(process.env.MESSAGING_SENDER_ID || require('./config').default.messagingSenderId),
+    appId: (process.env.APP_ID || require('./config').default.appId),
+    measurementId: (process.env.MEASUREMENT_ID || require('./config').default.measurementId)
 }
-console.log(appconfig)
+
 const app = firebase.initializeApp(appconfig)
 const auth = app.auth()
 export default auth
