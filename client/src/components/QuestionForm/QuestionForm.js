f0f8f5160b98cc234a50042e7b7e2c05711ca595
Tanner Ropp <tanner.ropp@ufl.edu>
2019-11-19 10:45:50 -0500

    created question component

---
+++ client/src/components/QuestionForm/QuestionForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import questionData from '../../assets/QuestionData.js'
+
+class QuestionForm extends React.Component {
+  render() {
+    const questionList = questionData.map(question => {
+      
+    })
+    return (
+
+    )
+  }
+}
+export default QuestionForm;
