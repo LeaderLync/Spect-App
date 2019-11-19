1ce1e3a765e581386dd869102d13214da0e2daca
Tanner Ropp <tanner.ropp@ufl.edu>
2019-11-19 18:00:36 -0500

    added skill attribute to questions

--- client/src/assets/QuestionData.js
+++ client/src/assets/QuestionData.js
@@ -1,92 +1,110 @@
 export default[
   {
     id: "question1",
-    question: "[LEADERSHIP]  A leader must not hold any grudges or biases against anyone in the team. Do you agree with the statement?",
+    skill: "Leadership",
+    question: "A leader must not hold any grudges or biases against anyone in the team. Do you agree with the statement?",
     option1: "Agree",
     option0: "Disagree"
   }, {
     id: "question2",
-    question: "[LEADERSHIP]  You are leading a meeting with a group of people and you have to make a decision about XYZ. Who makes the final decision?",
+    skill: "Leadership",
+    question: "You are leading a meeting with a group of people and you have to make a decision about XYZ. Who makes the final decision?",
     option1: "You do.  Since you are leading the meeting, your opinion is the only one that matters.",
     option0: "The group makes the final decision."
   }, {
     id: "question3",
-    question: "[TEAMWORK]  I think that teams perform best when individuals keep doing the same tasks and perfecting them, instead of learning new skills and challenging themselves",
+    skill: "Teamwork",
+    question: "I think that teams perform best when individuals keep doing the same tasks and perfecting them, instead of learning new skills and challenging themselves",
     option1: "Agree",
     option0: "Disagree"
   }, {
     id: "question4",
-    question: "[TEAMWORK]  Do you prefer working in teams or independently?",
+    skill: "Teamwork",
+    question: "Do you prefer working in teams or independently?",
     option1: "In teams",
     option0: "Independently"
   }, {
     id: "question5",
-    question: "[MINDFULNESS]  When I am focusing on a task/project, I tend to forget about the world/people around me.",
+    skill: "Mindfulness",
+    question: "When I am focusing on a task/project, I tend to forget about the world/people around me.",
     option1: "Agree",
     option0: "Disagree"
   }, {
     id: "question6",
-    question: "[MINDFULNESS]  I need to take mindful breaks to be more efficient throughout the day.",
+    skill: "Mindfulness",
+    question: "I need to take mindful breaks to be more efficient throughout the day.",
     option1: "Agree",
     option0: "Disagree"
   }, {
     id: "question7",
-    question: "[CREATIVITY]  I like to create a plan or an outline before I start a project.",
+    skill: "Creativity",
+    question: "I like to create a plan or an outline before I start a project.",
     option1: "Agree",
     option0: "Disagree"
   }, {
     id: "question8",
-    question: "[CREATIVITY]  When given a task or a project, do you:",
+    skill: "Creativity",
+    question: "When given a task or a project, do you:",
     option1: "Think in a logistical manner in finding small details about the project",
     option0: "Think about the different directions to take the project in on a big picture scale"
   }, {
     id: "question9",
-    question: "[WORK ETHIC]  How would you describe your work ethic?",
+    skill: "Work Ethic",
+    question: "How would you describe your work ethic?",
     option1: "I like to be productive and get as much done in the time that I am given",
     option0: "I like to be diligent and make sure I am producing high-quality work"
   }, {
     id: "question10",
-    question: "[WORK ETHIC]  If I am not passionate about a certain task/project, I do not put as much work into it.",
+    skill: "Work Ethic",
+    question: "If I am not passionate about a certain task/project, I do not put as much work into it.",
     option1: "Agree",
     option0: "Disagree"
   }, {
     id: "question11",
-    question: "[TIME MANAGEMENT]  In your opinion, what is more important, focusing on one thing, in great detail, or doing many different things at the same time?",
+    skill: "Time Management",
+    question: "In your opinion, what is more important, focusing on one thing, in great detail, or doing many different things at the same time?",
     option1: "One thing",
     option0: "Many things"
   }, {
     id: "question12",
-    question: "[TIME MANAGEMENT]  Your boss gives you a very important project that is due next week.  However, you have other assignments to do that are due this week and next week.  Do you:",
+    skill: "Time Management",
+    question: "Your boss gives you a very important project that is due next week.  However, you have other assignments to do that are due this week and next week.  Do you:",
     option1: "Solely focus on the project and ignore your other assignments",
     option0: "Finish your other assignments and start the project after everything is completed"
   }, {
     id: "question13",
-    question: "[GLOBAL AWARENESS]   It is important  to have a diverse (in thought and in race/ethnicity/gender/age/religion) group of people in the workplace.",
+    skill: "Global Awareness",
+    question: "It is important  to have a diverse (in thought and in race/ethnicity/gender/age/religion) group of people in the workplace.",
     option1: "Agree",
     option0: "Disagree"
   }, {
     id: "question14",
-    question: "[GLOBAL AWARENESS]  In a team, is it more important to have team members that are like-minded (and have the assignment done rather quickly) or have team members that have very different perspectives on how to achieve the assignment (and have the assignment take a long time to complete?",
+    skill: "Global Awareness",
+    question: "In a team, is it more important to have team members that are like-minded (and have the assignment done rather quickly) or have team members that have very different perspectives on how to achieve the assignment (and have the assignment take a long time to complete?",
     option1: "Like-minded team members",
     option0: "Diverse group of individuals with different perspectives"
   }, {
     id: "question15",
-    question: "[COMMUNICATION]  What is your preferred method of communication in the workplace?",
+    skill: "Communication",
+    question: "What is your preferred method of communication in the workplace?",
     option1: "Written communication (emaling, texting)",
     option0: "Verbal, in-person communication"
   }, {
     id: "question16",
-    question: "[COMMUNICATION]  I believe that my body language and facial expressions are not important and will not make a difference when talking to a coworker or my boss.",
+    skill: "Communication",
+    question: "I believe that my body language and facial expressions are not important and will not make a difference when talking to a coworker or my boss.",
     option1: "Agree",
     option0: "Disagree"
   }, {
     id: "question17",
-    question: "[CRITICAL THINKING]  There are seven employees presenting at a conference; Julia, Andrew, Penny, Bobby, Shayna, Jones, and Neil.  Each will present once and will go after another.  The order of presentations is under the following conditions:",
+    skill: "Critical Thinking",
+    question: "There are seven employees presenting at a conference; Julia, Andrew, Penny, Bobby, Shayna, Jones, and Neil.  Each will present once and will go after another.  The order of presentations is under the following conditions:  Julia must present at some time before Penny presents. Shayna must present at some time before Andrew presents. Jones must present either immediately before or immediately after Bobby presents. Neil must present third. If Penny is to present immediately before Shayna presents, Jones can present",
     option1: "Second",
     option0: "Third"
   }, {
     id: "question18",
-    question: "[CRITICAL THINKING]  I enjoy being challenged and learning from my mistakes.",
+    skill: "Critical Thinking",
+    question: "I enjoy being challenged and learning from my mistakes.",
     option1: "Agree",
     option0: "Disagree"
   }
