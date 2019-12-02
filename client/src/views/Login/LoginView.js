de363d80d361b57735c0a01a5234450556a272ea
Marc Diaz <marcgabe15@gmail.com>
2019-12-02 15:28:05 -0500

    changed admin to company

--- client/src/views/Login/LoginView.js
+++ client/src/views/Login/LoginView.js
@@ -15,12 +15,12 @@ class LoginView extends React.Component {
     (
     <div className="header-top">
         <Button variant="contained" color="primary">Student</Button>
-        <Button onClick={this.props.userUpdate} variant="contained">Admin</Button>
+        <Button onClick={this.props.userUpdate} variant="contained">Company</Button>
     </div>
     ) : (
       <div className="header-top">
       <Button onClick={this.props.userUpdate} variant="contained">Student</Button>
-      <Button variant="contained" color="primary">Admin</Button>
+      <Button variant="contained" color="primary">Company</Button>
       </div>
     )
     return (
