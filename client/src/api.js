995a89c0b74b7acedeeed9a8ac09637b5e7b61d0
Marc Diaz <marcgabe15@gmail.com>
2019-12-01 19:59:00 -0500

    added new url

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const client = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://limitless-dusk-18160.herokuapp.com/api',
     headers: {
       'accept': 'application/json',
       'content-type': 'application/json',
