048affe280b90ac2f8e84a332cdc7c36a4d284ac
Marc Diaz <marcgabe15@gmail.com>
2020-01-16 17:44:09 -0500

    changed login

--- client/src/views/Login/LoginView.js
+++ client/src/views/Login/LoginView.js
@@ -3,7 +3,7 @@ import './LoginPage.css'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/Black-logo-no-background.png'
 import Button from '@material-ui/core/Button'
-
+import PhoneImg from '../../assets/mobile-home-november.png'
 
 class LoginView extends React.Component {
   constructor(props) {
@@ -26,8 +26,8 @@ class LoginView extends React.Component {
     )
     return (
       <div className="login-page container-fluid">
-        <div className="row">
-          <div className="col">
+        <div className="row" style={{height: '100%'}}>
+          <div className="col-sm-6">
             {isStudent}
             <div className="login-modal">
                 <img className="spect-logo" src={logo}></img>
@@ -55,8 +55,15 @@ class LoginView extends React.Component {
                   </Link>
                 </form>
             </div>
-            <div>
-
+          </div>
+          <div class="col-sm-6" style={{backgroundColor: '#28a4eb'}}>
+            <img className="spect-logo" src={logo}></img>
+            <h3 style={{color:'white'}}>Now you can start tracking your soft skills and leadership potential to help you develop your best self!</h3>
+            <div style={{marginTop: '40px'}}>
+                <div class="row">
+                  <div class="col-sm-6" style={{maxHeight: '500px'}}><p style={{color: 'white'}}>Our process is simple. We want to get to know you better so we can recommend the best internship, company, or mentor for you</p></div>
+                  <div class="col-sm-6" style={{maxHeight: '500px'}}><img style={{height: '75%', margin: '0 auto'}} src={PhoneImg}/></div>
+                </div>
             </div>
           </div>
         </div>
