ea9e84526b946121c205f4015b0b735f1683eb9b
Tanner Ropp <tanner.ropp@ufl.edu>
2020-01-18 19:43:03 -0500

    for heroku

---
+++ server/config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+db: {
+uri: 'mongodb+srv://adminuser:Software4c!@spect-app-zkgek.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
+},
+databaseURL: 'https://spect-app.firebaseio.com'
+};
